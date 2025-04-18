683fff29f9ddd1dfd353346f997f5aadcedb397d
Autumn Willard <autumn@hey.com>
2025-04-18 15:30:34 -0500

    Basic student views added

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
+    has_one_attached :profile_picture
     validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "students#index"
   resources :students
 end
